1644b5522900b8998d1b9ad10949817323f53e12
Nathan Kidd <nathanblenheimkidd@gmail.com>
2024-12-28 14:43:53 +0000

    added popperjs support

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,6 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+window.process = { env: { NODE_ENV: 'production'} }
+
 import "@hotwired/turbo-rails"
 import "controllers"
-import * as bootstrap from "bootstrap"
+import "@popperjs/core"
+import 'bootstrap'
